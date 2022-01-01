4f8a933235d3b2ad8ce7d5208b8b1d6fd20c2546
kr-pentagon <96647530+kr-pentagon@users.noreply.github.com>
2022-01-01 16:35:14 +0900

    If tempurture goes down under 0 degrees it prints FROZEN

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 let 온도 = 0
 while (true) {
     if (온도 != input.temperature()) {
-        if (input.temperature() >= 40) {
+        if (input.temperature() > 40) {
             basic.showString("ERROR")
+        } else if (input.temperature() < 0) {
+            basic.showString("FROZEN")
         } else {
             basic.showNumber(input.temperature())
         }
