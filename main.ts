5dd6383f014b1ce1000323460f6a4122d11df51d
kr-pentagon <96647530+kr-pentagon@users.noreply.github.com>
2022-01-01 16:01:59 +0900

    If tempurture sensor value is up to 40 degrees prints ERROR sign and it will not print degrees.

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 let 온도 = 0
 while (true) {
     if (온도 != input.temperature()) {
-        basic.showNumber(input.temperature())
+        if (input.temperature() >= 40) {
+            basic.showString("ERROR")
+        } else {
+            basic.showNumber(input.temperature())
+        }
         온도 = input.temperature()
     }
 }
